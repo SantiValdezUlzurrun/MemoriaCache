9760ed36188ad877b240499a412c9103c6aafebb
Santiago Valdez <santiagoval1999@gmail.com>
2021-06-18 23:36:19 -0300

    feat: interprete

---
+++ src/interprete.c
@@ -0,0 +1,77 @@
+#include "interprete.h"
+
+/* Para desechar la línea en caso de error */
+#define MAX_LINEA 100
+
+bool interprete_crear(interprete_t* interprete, char* archivo_entrada, char* archivo_salida){
+	if (!interprete) return ERROR;
+
+	if (!(interprete->archivo_entrada = fopen(archivo_entrada, "r")) || 
+	 	!(interprete->archivo_salida = fopen(archivo_salida, "w")))
+		return ERROR;
+	
+	return EXITO;
+}
+/*
+
+void escribir(interprete_t* inter){
+	int direccion = 0;
+	unsigned char byte = 0;
+	fscanf(inter->archivo_entrada, "%i,%hhu\n", &direccion, &byte);
+	write_byte(direccion, byte);
+	if(error){
+		fprintf(stderr, "Ocurrio un error: la direccion ingresada no es valida(%i)\n", direccion);
+	}else{
+		fprintf(inter->archivo_salida, "Escritura:\tDireccion(%05i)\t\tValor(%hhu)\t%s\n", direccion, byte, cache.hit?"Hit":"Miss");
+	}
+}
+
+void leer(interprete_t* inter){
+	int direccion = 0;
+	fscanf(inter->archivo_entrada, "%i\n",&direccion);
+	unsigned char byte = read_byte(direccion);
+	if(error){
+		fprintf(stderr, "Ocurrio un error: la direccion ingresada no es valida(%i)\n", direccion);
+	}else{
+		fprintf(inter->archivo_salida,"Lectura:\tDireccion(%05i)\t\tValor(%hhu)\t%s\n", direccion, byte, cache.hit?"Hit":"Miss");
+	}
+}
+*/
+
+void interprete_interpretar(interprete_t* inter){
+
+	char instruccion[5] = "";
+	int i = 1;
+	char tacho[MAX_LINEA];
+	while(!feof(inter->archivo_entrada)){
+		fscanf(inter->archivo_entrada,"%s", instruccion);
+		if(strcmp(instruccion, "init") == 0){
+			init();
+		}else if(strcmp(instruccion, "MR") == 0){
+			int miss_rate = get_miss_rate();
+			fprintf(inter->archivo_salida, "Miss rate: %i%%\n", miss_rate);
+		}else if (strcmp(instruccion, "R") == 0){
+			leer(inter);
+		}else if (strcmp(instruccion, "W") == 0){
+			escribir(inter);
+		}else if(strlen(instruccion) == 0){
+		}else{
+			fscanf(inter->archivo_entrada, "%[^\n]\n", tacho);
+			fprintf(stderr, "Error de sintaxis. Linea: %i\n", i);
+		}
+		strcpy(instruccion, "");
+		i++;
+	}
+}
+
+bool interprete_destruir(interprete_t* interprete) {
+	if (!interprete) return ERROR;
+
+	if (fclose(interprete->archivo_entrada) ||
+		fclose(interprete->archivo_salida)) 
+		return ERROR;
+	
+	return EXITO;
+}
+
+

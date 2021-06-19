4a9b466d8db1d4e8befd628ed8af9ff8d1be0612
MauricioBuzzone <mbuzzone@fi.uba.ar>
2021-06-18 23:48:04 -0300

    funciones find

--- src/cache.c
+++ src/cache.c
@@ -3,71 +3,35 @@
 #include <math.h>
 
 
-bloque_t bloque_crear(){
-	bloque_t bloque;
-	bloque.valido = false;
-	bloque.dirty = false;
-	bloque.direccion = 0x0;
-	bloque.datos = malloc(cache.tamanio_bloque*sizeof(char));
-	return bloque;
-}
-
-
-
-
-bloque_t* inicializar_bloques(int cant_bloques){
-	bloque_t* bloque = malloc(cant_bloques * sizeof(bloque_t));
-	if(!bloque) return NULL;
-		
-	for (int j = 0; j < cant_bloques; j++){
-		bloque[j] = bloque_crear();
-	}
-	return bloque;
-}
-
-
-via_t* inicializar_vias(int cant_vias,int cant_bloques){
-	via_t* vias = malloc(cant_vias * sizeof(via_t));
-	if(!vias) return NULL;
-
-	for(int i = 0; i < cant_vias; i++){
-		vias[i].bloques = inicializar_bloques();
-		if(!vias[i].bloques){
-			free(vias);
-			return NULL;
-		}
-	}
-
-	vias.cant_bloques = cant_bloques;
-	return vias;
-}
 
 //Devuelve la cantidad de bits para representar el numero dado
 int bits_para_representar(int numero){
 	return (int) ceil(log2(numero));
 }
 
-void init(){
-	if(cache.inicializada) return;
+void init();
 
-	int tamanio_vias = cache.tamanio_cache/cache.cant_vias;
-	int cant_bloques = tamanio_vias/cache.tamanio_bloque;
 
-	cache.bits_tag = bits_para_representar(BITS_DIRECCION - cache.cant_vias - cant_bloques);
-	cache.bits_index = bits_para_representar(cache.cant_vias);
-	cache.bits_offset = bits_para_representar(cant_bloques);
-	cache.hits = 0;
-	cache.misses = 0;
-	cache.inicializada = true;	
-
-	cache.vias = inicializar_vias(cache.cant_vias ,cant_bloques);
+unsigned int find_set(int address){
+	unsigned short index = address << cache.bits_tag;
+  	index = index >> (cache.bits_offset + cache.bits_tag);
+  	return index;
 }
 
+unsigned int find_earliest(int setnum){
+	if(0 < setnum && setnum < cache.tamanio_cache/(cache.cant_vias * cache.tamanio_bloque)) return setnum;
 
+	int ant_actual = cache.vias[0].bloques[setnum].antiguedad;
+	for (int i = 1; i < cache.cant_vias; i++){
+		int ant_bloque = cache.vias[i].bloques[setnum].antiguedad;
+		if(ant_bloque == 0) return i;
 
-unsigned int find_set(int address){}
-
-unsigned int find_earliest(int setnum){}
+		if(ant_bloque < ant_actual){
+			ant_actual = ant_bloque;
+		}
+	}
+	return ant_actual;
+}
 
 void read_block(int blocknum){}
 
@@ -84,4 +48,11 @@ char get_miss_rate(){
 	return (cache.misses*100)/(accesos);
 }
 
-void cache_destruir(){}
+/*
+	Via-0		Via-1		Via-2		Via-3
+	| A	|		| A	|		| A	|		| A	|
+	| B	|		| B	|		| B	|		| B	|
+	| C	|		| C	|		| C	|		| C	|
+	| D	|		| D	|		| D	|		| D	|
+	
+*/
